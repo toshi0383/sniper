bc24df3948e823399d037413f1c83674feada8c4
toshi0383 <t.suzuki326@gmail.com>
2015-11-07 20:25:20 +0900

    version 0.2.0

--- sniper/main.swift
+++ sniper/main.swift
@@ -12,7 +12,7 @@ let UnknownBundleID = "<unknown bundle ID>"
 
 CLI.setup(
            name: "sniper",
-        version: "0.1.1",
+        version: "0.2.0",
     description: "the OSX app terminator"
 )
 /**
